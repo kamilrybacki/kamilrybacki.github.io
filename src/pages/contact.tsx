e802364129da0d406a123ff18a4fa3ce1cfc8d13
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2022-05-08 19:13:28 +0200

    added contact form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,14 @@
 import  React from "react"
 
 import PageWrapper from "@components/PageWrapper" 
+import SocialMediaBar from "@components/SocialMediaBar"
+
+// Contact form id: d2f32602d5a315ecf57a8ddf8c45f3b9
 
 const ContactPage = () => {
   return (
-	<PageWrapper>
+	<PageWrapper footer={false}>
+		<SocialMediaBar/>
 	</PageWrapper>
   )
 }
