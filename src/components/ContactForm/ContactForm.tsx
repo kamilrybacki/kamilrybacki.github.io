ff8e19188fdf2968363325e48396fd091e7677d9
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2022-05-09 09:30:19 +0200

    added contatc form

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -6,14 +6,64 @@ type ContactFormProps = {
 }
 
 const ContactFormWrapper = tw.form`
+	relative
 	flex
 	flex-col
+	gap-2
+	w-1/3
+	my-5
+`
+
+const ContactInputLabel = tw.span`
+	text-sm
+	text-primary-400
+	underline
+	underline-offset-2
+	decoration-accent-500
+	decoration-dotted
+	decoration-2
+`
+
+const ContactFormInput = tw.input`
+	border-2
+	border-primary-500
+	placeholder-primary-200
+	py-1
+	px-2
+	mb-2
+`
+
+const ContactFormMessage = tw.textarea`
+	border-2
+	border-primary-500
+	placeholder-primary-200
+	p-2
+	overflow-y-scroll
+	scrollbar-thin
+`
+
+const ContactFormSendButton = tw.button`
+	relative
+	bg-accent-500
+	w-20
+	h-10
+	text-secondary-500
+	font-display
+	tracking-wide
+	text-2xl
+	uppercase
 `
 
 const ContactForm: React.FunctionComponent<ContactFormProps> = ({endpoint}) => {
 	return(
 		<ContactFormWrapper action={endpoint} method="POST">
-			
+			<ContactInputLabel>Name</ContactInputLabel>
+			<ContactFormInput type="text" name="name" placeholder="Required" required/>
+			<ContactInputLabel>Email</ContactInputLabel>
+			<ContactFormInput type="email" name="email" placeholder="Optional"/>
+			<ContactInputLabel>Your message</ContactInputLabel>
+			<ContactFormMessage name="message" rows={15} placeholder="Required" required/>
+			<ContactFormSendButton type="submit">Send</ContactFormSendButton>
 		</ContactFormWrapper>
 	)
 }
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,12 +2,14 @@ import  React from "react"
 
 import PageWrapper from "@components/PageWrapper" 
 import SocialMediaBar from "@components/SocialMediaBar"
+import ContactForm from "@components/ContactForm"
 
-// Contact form id: d2f32602d5a315ecf57a8ddf8c45f3b9
+// Contact form id: 
 
 const ContactPage = () => {
   return (
 	<PageWrapper footer={false}>
+		<ContactForm endpoint="https://formsubmit.co/d2f32602d5a315ecf57a8ddf8c45f3b9"/>
 		<SocialMediaBar/>
 	</PageWrapper>
   )
