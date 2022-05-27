28d56e32eb3dc6e801dd1d1eb0476b74c01c4967
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2022-05-27 15:06:00 +0200

    removed ssr lines from browser config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,1 @@
 import './src/style/global.css';
-
-export const onPreRenderHTML = ({getHeadComponents, replaceHeadComponents}) => {
-  /**
-     * @type {any[]} headComponents
-     */
-  const headComponents = getHeadComponents();
-
-  headComponents.sort((a, b) => {
-    if (a.props && a.props['data-react-helmet']) {
-      return 0;
-    }
-    return 1;
-  });
-  replaceHeadComponents(headComponents);
-};
