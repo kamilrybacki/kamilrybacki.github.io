669df7f08951591d14af2fa64fbf0f86cedb3eac
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2022-05-28 12:11:09 +0200

    wrong destructuring fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,17 +1,27 @@
 const {Helmet} = require('react-helmet');
 
 exports.onRenderBody = (
-    {setHeadComponents, setHtmlAttributes, setBodyAttributes,
-      getHeadComponents},
+    {setHeadComponents, setHtmlAttributes, setBodyAttributes},
 ) => {
   const helmet = Helmet.renderStatic();
+  setHtmlAttributes(helmet.htmlAttributes.toComponent());
+  setBodyAttributes(helmet.bodyAttributes.toComponent());
+  setHeadComponents([
+    helmet.title.toComponent(),
+    helmet.link.toComponent(),
+    helmet.meta.toComponent(),
+    helmet.noscript.toComponent(),
+    helmet.script.toComponent(),
+    helmet.style.toComponent(),
+  ]);
+};
+
+exports.onPreRenderHTML = ({getHeadComponents, replaceHeadComponents}) => {
   const headComponents = getHeadComponents();
   const orderedHeadComponents = headComponents.sort((a, b) => {
     if (a.type === 'meta') return -1;
     if (a.type === b.type && a.type === 'meta') return 0;
     return 1;
   });
-  setHtmlAttributes(helmet.htmlAttributes.toComponent());
-  setHeadComponents(orderedHeadComponents);
-  setBodyAttributes(helmet.bodyAttributes.toComponent());
+  replaceHeadComponents(orderedHeadComponents);
 };
