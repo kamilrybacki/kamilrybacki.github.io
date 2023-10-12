fc0deef5e542c5bca19ef67a69f4ad93b7d39cde
kamilrybacki <76902941+kamilrybacki@users.noreply.github.com>
2023-10-12 05:28:53 +0000

    Build: (f870409) update post patch

--- public/post.js
+++ public/post.js
@@ -20,6 +20,7 @@ function waitForElementToExist(selector) {
 
 const topPanelId = '#top-panel-wrapper';
 const menuPanelId = '#menu-panel-wrapper';
+const notebookContentsClass = ".jp-Notebook"
 
 const hideElement = async (selector) => {
   await waitForElementToExist(selector)
@@ -28,9 +29,23 @@ const hideElement = async (selector) => {
     });
 }
 
+const applyBaseSiteTheme = (colors) => {
+  const rootElement = document.querySelector(":root");
+  rootElement.style.setProperty('--jp-layout-color2', colors.background);
+  rootElement.style.setProperty('--jp-layout-color0', colors.background);
+  rootElement.style.setProperty('--jp-toolbar-background', colors.background);
+  rootElement.style.setProperty('--jp-ui-font-color1', colors.foreground);
+  rootElement.style.setProperty('--jp-content-font-color0', colors.foreground);
+  rootElement.style.setProperty('--jp-content-font-color1', colors.foreground);
+}
+
 async function post () {
   await hideElement(topPanelId);
   await hideElement(menuPanelId);
+  await waitForElementToExist(notebookContentsClass)
+    .then(() => import('./theme.js'))
+    .then((m) => m.theme)
+    .then(({ colors }) => applyBaseSiteTheme(colors));
   console.log('Finished post script');
 };
 
