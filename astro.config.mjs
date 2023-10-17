f9a39a8e9a64dd8bac927f1cb498efd38ad5e2cf
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-10-17 20:31:01 +0200

    add seo integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,20 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import remarkCodeTitles from "remark-code-titles";
+import robotsTxt from "astro-robots-txt";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://kamilrybacki.github.io/",
-    integrations: [
-        mdx({
-          remarkPlugins: [
-            remarkCodeTitles
-          ]
-        }),
-        tailwind({
-            config: {
-                applyBaseStyles: false,
-            },
-        }),
-        react(),
-    ],
+  site: "https://kamilrybacki.github.io/",
+  integrations: [mdx({
+    remarkPlugins: [remarkCodeTitles]
+  }), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), react(), robotsTxt(), compress()]
 });
