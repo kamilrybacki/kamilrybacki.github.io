e034841999e90644c93b3e716d70658aec8d7cd2
Kamil Rybacki <krybacki@task.gda.pl>
2023-12-29 15:24:24 +0100

    ave

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkCodeTitles from "remark-code-titles";
 import robotsTxt from "astro-robots-txt";
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 import sitemap from "@astrojs/sitemap";
 
@@ -11,7 +13,8 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://kamilrybacki.github.io/",
   integrations: [mdx({
-    remarkPlugins: [remarkCodeTitles]
+    remarkPlugins: [remarkCodeTitles, remarkMath],
+    rehypePlugins: [rehypeKatex]
   }), tailwind({
     config: {
       applyBaseStyles: false
